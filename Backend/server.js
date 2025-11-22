29446003a590075d50d1f86570bfd8bfe81c7bfd
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-11-22 16:39:23 +0100

    installed the cors files and added the missing code in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Backend is working");
 });
 
+app.post("/register", (req, res) => {
+	const {username, password} = req.body;
+	console.log("Received:", username, password);
+	res.json({message: "Register request received!"});
+});
+
 const PORT = 5000;
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
