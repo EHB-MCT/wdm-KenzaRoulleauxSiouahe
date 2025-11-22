e29accd2a4ac648db8f3babb6c084e4561677131
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-11-22 16:54:37 +0100

    added the mongodb code to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = "mongodb+srv://kenzaroulleauxsiouahe_db_user:Mc5mMTyPm2XtKcFF@heartappcluster.skpvnfq.mongodb.net/?appName=HeartAppcluster";
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		await client.connect();
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,9 +34,9 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-	const {username, password} = req.body;
+	const { username, password } = req.body;
 	console.log("Received:", username, password);
-	res.json({message: "Register request received!"});
+	res.json({ message: "Register request received!" });
 });
 
 const PORT = 5000;
