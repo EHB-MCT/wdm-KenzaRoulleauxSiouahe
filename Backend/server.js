56cff4274c390243676645eb119fd64eb5de8722
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-11-26 17:21:24 +0100

    added the login route in "server.js" in backend but its not working yet going to wrok on it later

--- Backend/server.js
+++ Backend/server.js
@@ -52,15 +52,37 @@ app.post("/register", async (req, res) => {
 	const existingUser = await usersCollection.findOne({ username });
 
 	if (existingUser) {
-		return res
-			.status(400)
-			.json({ message: "Username already exists. Try another." });
+		return res.status(400).json({ message: "Username already exists. Try another." });
 	}
 	await usersCollection.insertOne({ username, password });
 
 	res.json({ message: "User registered successfully!" });
 });
 
+//Login route
+app.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	if (!usersCollection) {
+		return res.status(500).json({ message: "Database not connected yet" });
+	}
+	if (!username || !password) {
+		return res.status(400).json({ message: "Please provide username and password." });
+	}
+	
+	//Find user in DB
+	const user = await usersCollection.findOne({ username });
+
+	if (!user) {
+		return res.status(400).json({ message: "User not found" });
+	}
+
+	//Check password
+	if (user.password !== password) {
+		return res.status(400).json({ message: "Incorrect password" });
+	}
+
+	res.json({ message: "Login successful!" });
+});
 const PORT = 5000;
 
 app.listen(PORT, () => {
