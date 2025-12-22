e8f36ff30117fce6d68529b5680b2a9d21de425d
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-12-22 15:18:13 +0100

    added an id to the search header to make it dissapear when the user is searcvhing something, added the moviescontainer and searchheader to the js file and also loaded a the posters of the default movies that will show when the users goes to search a movie + to block the searchheader when the user is searching a movie

--- Frontend/movie.js
+++ Frontend/movie.js
@@ -1,8 +1,33 @@
 const searchInput = document.getElementById("searchInput");
 const resultsDiv = document.getElementById("results");
+const moviesContainer = document.getElementById("moviesContainer");
+const searchHeader = document.getElementById("searchHeader");
+
+globalThis.addEventListener("DOMContentLoaded", async () => {
+	const response = await fetch("http://localhost:5000/movies?q=");
+	const movies = await response.json();
+	movies.forEach((movie) => {
+		const item = document.createElement("div");
+		item.classList.add("movie-card-default");
+
+		item.innerHTML = `
+      <img src="http://localhost:5000${movie.Poster}" alt="${movie.Title}" />
+      <h4>${movie.Title}</h4>
+    `;
+
+		moviesContainer.appendChild(item);
+	});
+});
 
 searchInput.addEventListener("input", async () => {
 	const query = searchInput.value.trim();
+	if (query === "") {
+		searchHeader.style.display = "block";
+		moviesContainer.style.display = "flex";
+	} else {
+		searchHeader.style.display = "none";
+		moviesContainer.style.display = "none";
+	}
 
 	const response = await fetch("http://localhost:5000/movies?q=" + encodeURIComponent(query));
 	const movies = await response.json();
