b8ed6e460c540928c1fbda791ee8e0d6a9b3c725
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-12-23 17:28:28 +0100

    added the css and js  of the profile setup

---
+++ Frontend/profile-setup.js
@@ -0,0 +1,39 @@
+const saveProfileBtn = document.getElementById("saveProfileBtn");
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get("username");
+
+saveProfileBtn.addEventListener("click", async () => {
+	const displayName = document.getElementById("displayName").value;
+	const fearLevel = document.getElementById("fearLevel").value;
+
+	const selectedGenres = Array.from(document.querySelectorAll(".genres input:checked")).map((el) => el.value);
+
+	if (!displayName) {
+		alert("Please choose a display name");
+		return;
+	}
+
+	try {
+		const res = await fetch("http://localhost:5000/profile-setup", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				username,
+				displayName,
+				fearLevel,
+				favoriteGenres: selectedGenres,
+			}),
+		});
+
+		const data = await res.json();
+		if (!res.ok) {
+			alert(data.message);
+			return;
+		}
+
+		window.location.href = "homepage.html";
+	} catch (err) {
+		console.error(err);
+		alert("Could not save profile");
+	}
+});
