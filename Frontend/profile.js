fcca12ea5cebadd67300ce56d0abf93532b0fa5a
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-12-25 16:05:35 +0100

    added the loggedUser and loadProfile func to display the usernma of the user that just registered

--- Frontend/profile.js
+++ Frontend/profile.js
@@ -1,5 +1,7 @@
 const tabButtons = document.querySelectorAll(".tab-btn");
 const sections = document.querySelectorAll(".movies-section");
+const email = localStorage.getItem("loggedUser");
+
 
 tabButtons.forEach((btn) => {
 	btn.addEventListener("click", () => {
@@ -12,3 +14,26 @@ tabButtons.forEach((btn) => {
 		document.getElementById(btn.dataset.tab).classList.add("active");
 	});
 });
+
+if (!email) {
+	window.location.href = "login.html";
+}
+
+async function loadProfile() {
+	try {
+		const res = await fetch(
+			`http://localhost:5000/profile?email=${encodeURIComponent (email)}`
+		);
+		if (!res.ok){
+			throw new Error ("Failed to fetch profile");
+		}
+		const user = await res.json();
+
+		document.querySelector(".username").textContent = 
+		user.displayName || "Unnamed user";
+
+	} catch(Error){
+		console.error("Profile load error: Error");
+	}
+}
+loadProfile();
