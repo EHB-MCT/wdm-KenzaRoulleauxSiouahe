b6af7e31ee5127dd2410176dedb8b35c369cfc75
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2025-12-25 17:42:33 +0100

    forgot to add the library of icons so i did it now in profile html, changed the profile pic to load the pic uploaded, addedf the users avatar fetching in profile.js, corrected a typing error in profile.js "Error "-> "error"

--- Frontend/profile.js
+++ Frontend/profile.js
@@ -2,38 +2,37 @@ const tabButtons = document.querySelectorAll(".tab-btn");
 const sections = document.querySelectorAll(".movies-section");
 const email = localStorage.getItem("loggedUser");
 
+if (!email) {
+	window.location.href = "login.html";
+}
 
 tabButtons.forEach((btn) => {
 	btn.addEventListener("click", () => {
-		
 		tabButtons.forEach((b) => b.classList.remove("active"));
 		sections.forEach((s) => s.classList.remove("active"));
 
-		
 		btn.classList.add("active");
 		document.getElementById(btn.dataset.tab).classList.add("active");
 	});
 });
 
-if (!email) {
-	window.location.href = "login.html";
-}
-
 async function loadProfile() {
 	try {
-		const res = await fetch(
-			`http://localhost:5000/profile?email=${encodeURIComponent (email)}`
-		);
-		if (!res.ok){
-			throw new Error ("Failed to fetch profile");
+		const res = await fetch(`http://localhost:5000/profile?email=${encodeURIComponent(email)}`);
+		if (!res.ok) {
+			throw new Error("Failed to fetch profile");
 		}
 		const user = await res.json();
 
-		document.querySelector(".username").textContent = 
-		user.displayName || "Unnamed user";
+		document.querySelector(".username").textContent = user.displayName || "Unnamed user";
 
-	} catch(Error){
-		console.error("Profile load error: Error");
+		if (user.avatar) {
+			document.querySelector(".profile-pic").src = `http://localhost:5000${user.avatar}`;
+		} else {
+			document.querySelector(".profile-pic").src = "default-avatar.jpg";
+		}
+	} catch (error) {
+		console.error("Profile load error:", error);
 	}
 }
 loadProfile();
